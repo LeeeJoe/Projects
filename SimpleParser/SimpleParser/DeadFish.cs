642763ed3294985a3b3e9f3be34be38175fee38e
LeeeJoe <32437965+LeeeJoe@users.noreply.github.com>
2019-03-21 15:27:39 +0300

    Training EPAM Spring.2019 Classwork 09 from 2019/03/20 additional

---
+++ SimpleParser/SimpleParser/DeadFish.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleParser
+{
+    public class DeadFish
+    {
+        private List<int> listInt = new List<int>();
+        private string strToParse = "";
+        private int Count = 0;
+
+        public DeadFish()
+        {
+
+        }
+
+        public DeadFish(string stringToParse)
+        {
+            strToParse = stringToParse;
+        }
+
+        public int[] Parse()
+        {
+            DoParse();
+
+            return listInt.ToArray();
+        }
+
+        public int[] Parse(string stringToParse)
+        {
+            strToParse = stringToParse;
+
+            DoParse();
+
+            return listInt.ToArray();
+        }
+
+        private void DoParse()
+        {
+            for (int i = 0; i < strToParse.Length; i++)
+            {
+                switch (strToParse[i])
+                {
+                    case 'd':
+                        Dec();
+                        break;
+                    case 'i':
+                        Inc();
+                        break;
+                    case 's':
+                        Square();
+                        break;
+                    case 'o':
+                        AddToList();
+                        break;
+                }
+            }
+        }
+
+        private void Dec()
+        {
+            Count--;
+        }
+
+        private void Inc()
+        {
+            Count++;
+        }
+
+        private void Square()
+        {
+            Count *= Count;
+        }
+
+        private void AddToList()
+        {
+            listInt.Add(Count);
+        }
+    }
+}
---
+++ SimpleParser/SimpleParser/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleParser
+{
+    class Program
+    {
+        static void ShowArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i].ToString().PadLeft(4));
+            }
+            Console.WriteLine();
+        }
+
+
+        static void Main(string[] args)
+        {
+            DeadFish deadFish = new DeadFish();
+
+            int[] Arr = deadFish.Parse("iiisdoso");
+
+            ShowArray(Arr);
+
+
+            DeadFish deadFish2 = new DeadFish("ixicisdioso");
+
+            Arr = deadFish2.Parse();
+
+            ShowArray(Arr);
+        }
+    }
+}
